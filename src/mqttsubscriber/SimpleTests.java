62bbfc49dad566a068b79cdf12c9ca2a008ba190
John <Johnschroedercs@gmail.com>
2021-01-06 10:54:20 -0700

    Updating with simple pass and fail unit tests.

---
+++ src/mqttsubscriber/SimpleTests.java
@@ -0,0 +1,19 @@
+package mqttsubscriber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTests {
+
+	@Test
+	void passTest() {
+		assertTrue(true);
+	}
+
+	@Test
+	void failTest() {
+		assertTrue(false);
+	}
+
+}
